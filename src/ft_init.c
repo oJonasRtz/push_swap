cc0f2ca578d7af481f5aa049ceab997a09d7c20a
Jonas Alberto Pereira <jopereir@c1r1p2.42sp.org.br>
2024-12-09 17:12:42 -0300

    .

--- src/ft_init.c
+++ src/ft_init.c
@@ -6,49 +6,49 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 13:25:28 by jopereir          #+#    #+#             */
-/*   Updated: 2024/12/06 12:47:51 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/12/06 12:59:23 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// int	*stack_init(char *s, int *size)
-// {
-// 	int	*stack;
-// 	int	i;
-// 	int	j;
-
-// 	i = 1;
-// 	while (s[i])
-// 		i++;
-// 	*size = i - 1;
-// 	stack = ft_calloc(i, sizeof(int));
-// 	if (!stack)
-// 		return (NULL);
-// 	i = 1;
-// 	j = 0;
-// 	while (s[i])
-// 		stack[j++] = ft_atoi(s[i++]);
-// 	return (stack);
-// }
-
-int	*stack_init(char *s, int *size)
+int	*stack_init(char **s, int *size)
 {
-	int		*stack;
-	char	**split;
-	int		i;
+	int	*stack;
+	int	i;
+	int	j;
 
-	split = ft_split(s, ' ');
-	i = 0;
-	while (split[i])
+	i = 1;
+	while (s[i])
 		i++;
-	*size = i;
+	*size = i - 1;
 	stack = ft_calloc(i, sizeof(int));
-	i = 0;
-	while (split[i])
-	{
-		stack[i] = ft_atoi(split[i]);
-		i++;
-	}
+	if (!stack)
+		return (NULL);
+	i = 1;
+	j = 0;
+	while (s[i])
+		stack[j++] = ft_atoi(s[i++]);
 	return (stack);
 }
+
+// int	*stack_init(char *s, int *size)
+// {
+// 	int		*stack;
+// 	char	**split;
+// 	int		i;
+
+// 	split = ft_split(s, ' ');
+// 	i = 0;
+// 	while (split[i])
+// 		i++;
+// 	*size = i;
+// 	stack = ft_calloc(i, sizeof(int));
+// 	i = 0;
+// 	while (split[i])
+// 	{
+// 		stack[i] = ft_atoi(split[i]);
+// 		i++;
+// 	}
+// 	return (stack);
+// }
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 10:42:42 by jopereir          #+#    #+#             */
-/*   Updated: 2024/12/06 12:49:45 by jopereir         ###   ########.fr       */
+/*   Updated: 2024/12/06 13:00:09 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,13 +37,10 @@ int	main(int argc, char *argv[])
 	if (!isalldigit(&argv[1]) || isduplicate(&argv[1])
 		|| islargerthan_limits(&argv[1]))
 		exit(ft_printf("Error\n"));
-	stack.a = stack_init(argv[1], &stack.size_a);
+	stack.a = stack_init(argv, &stack.size_a);
 	stack.b = NULL;
 	stack.size_b = 0;
-	int i = 0;
-	while (i < stack.size_a)
-		ft_printf("%d\n", stack.a[i++]);	
-	// ft_least_moves(&stack);
+	ft_least_moves(&stack);
 	ft_double_free(stack.a, stack.b);
 	return (0);
 }
